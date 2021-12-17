5301dca3ce11779e734b4223f85d38ea9eeccfb0
Andrei Tirsina <andrei.tirsina@amdaris.com>
2021-12-17 02:09:58 +0200

    fix server

--- Pad3/Program.cs
+++ Pad3/Program.cs
@@ -21,12 +21,22 @@ var app = builder.Build();
 var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 db.Database.Migrate();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
